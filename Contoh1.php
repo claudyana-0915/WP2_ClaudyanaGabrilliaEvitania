23905ecf784b49bf787a95ab84235db2f78b86c7
claudyana-0915 <101854682+claudyana-0915@users.noreply.github.com>
2022-03-21 16:15:34 +0700

    Contoh1

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama Saya Claudyana Gabrillia Evitania
+            Tinggal di daerah Cilebut
+            Saya dari kampus BSI Margonda
+            Dengan NIM 17190915";
+        }
+}

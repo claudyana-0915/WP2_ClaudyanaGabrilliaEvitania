e23b62f90e501e9478217c59324a69c04d971cdc
claudyana-0915 <claudyanahaumahu9i@gmail.com>
2022-04-05 12:11:01 +0700

    menambahkan web.php, v_about.php, v_footer.php, v_header.php, v_index.php, css

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Lengkap : Claudyana Gabrillia Evitania</li>
+        <li>Nama Panggilan : Claudya</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Perumahan Taman Griya Kencana no.5</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Rumah Sakit</li>
+    </ul>
+
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Membaca</li>
+        <li>Dengerin Lagu</li>
+    </ul>
+</section> 
